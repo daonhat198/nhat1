c412bc039812a29e1ccd5aaacd9e13aafa65271d
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-03-02 10:56:35 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+    body {
+        background-image: url(https://vcdn-dulich.vnecdn.net/2021/12/24/An-Giang-0-jpeg-1470-1640315739.jpg);
+        text-align: center; 
+        background-size: cover;  
+        background-repeat: no-repeat;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        font-size: 20px;
+        font-family: Arial, Helvetica, sans-serif;
+        height: 100%;
+        width: 100%;
+        margin: 0%;
+    }
+
+    .custom-button {
+        color: white;
+        background-color: rgb(190, 43, 43);
+        width: 300px;
+        height: 50px;
+        border-radius: 5px;
+        text-align: center;
+        transition: all 0.3s ease;
+        border: none;
+        cursor: pointer;
+        box-shadow: 10px 20px rgba(red);
+        transform: scale(1.1);
+    }
+
+    .custom-button:hover {
+        background-color: red;
+    }
+
+    .container {
+        padding-top: 250px;
+        justify-content: center;
+    }
+
+    .hello {
+        padding-top: 30px;
+        justify-content: center;
+    }
+
+</style> 
+</head>
+<body>
+    <div class="container">
+        <h1>Don't miss this special offer!</h1>
+        <p>Get it now for just $10! The price will be increased after 50 downloads</p>
+        <div class="hello">
+            <p>
+                <a href="https://memart.vn/tin-tuc/blog1/huong-dan-unlock-bootloader-xiaomi-nhung-dieu-ban-can-biet-vi-cb.html" target="_blank" class="custom-button">Get the latest version of Bootslander</a>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
+
+`
